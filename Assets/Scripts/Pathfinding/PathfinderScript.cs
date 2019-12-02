dcdc29b0ac9a8ba67c786979c3782099549539be
Josh <joshmh7128@gmail.com>
2019-12-02 18:46:28 -0500

    working on AI

--- Assets/Scripts/Pathfinding/PathfinderScript.cs
+++ Assets/Scripts/Pathfinding/PathfinderScript.cs
@@ -9,25 +9,39 @@ public class PathfinderScript : MonoBehaviour
 
     public Vector3 targetPos;
     public int endPointInt;
+    public bool isLeaving;
 
     private void Start()
     {
         // add our starting position to the start pos list
         pathfindingManagerScript.StartPosition = transform;
-
-        // figure out where we're going
-        //endPointInt = Random.Range(0, pathfindingManagerScript.EndPoints.Length);
-        //Debug.Log("End Points List is " + pathfindingManagerScript.EndPoints.Length + " points long. " + "Set end point to: " + endPointInt);
+        isLeaving = false; // we're not leaving
     }
 
     // Update is called once per frame
     void Update()
     {
-        pathfindingManagerScript.endPointChoose = endPointInt;
-        pathfindingGrid.showPath = endPointInt;
-        // get our target position
-        List<Node> FinalPath = pathfindingGrid.FinalPaths[endPointInt];
-        targetPos = FinalPath[0].nodePos;
-        transform.position = Vector3.MoveTowards(transform.position, targetPos, 0.025f);
+        if (!isLeaving)
+        {
+            pathfindingManagerScript.endPointChoose = endPointInt;
+            pathfindingGrid.showPath = endPointInt;
+            // get our target position
+            List<Node> FinalPath = pathfindingGrid.FinalPaths[endPointInt];
+            targetPos = FinalPath[0].nodePos;
+            transform.position = Vector3.MoveTowards(transform.position, targetPos, 0.025f);
+        }
+        else
+        {
+
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "EndPoint")
+        {
+            isLeaving = true;
+
+        }
     }
 }
