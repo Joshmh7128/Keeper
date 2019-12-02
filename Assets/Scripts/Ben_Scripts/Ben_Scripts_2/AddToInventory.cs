462599488e1b8ba2ef26c8d651146964f19c4c7d
Ben Guzik <benjamin.guzik@uconn.edu>
2019-12-02 18:50:51 -0500

    Inventory Updates

---
+++ Assets/Scripts/Ben_Scripts/Ben_Scripts_2/AddToInventory.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddToInventory : MonoBehaviour
+{
+    public InventoryScript inventory;
+    public GameObject itemButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void AddInventory()
+    {
+        for (int i = 0; i < inventory.slots.Length; i++)
+        {
+            //checks if inventory is full
+            if (inventory.isFull[i] == false)
+            {
+                //Item is added to inv
+                inventory.isFull[i] = true;
+                //check if part is already in inventory, would need to stack
+                GameObject item = Instantiate(itemButton, inventory.slots[i].transform, false);
+                item.SetActive(true);
+                break;
+            }
+        }
+    }
+}
--- Assets/Scripts/Ben_Scripts/InventoryManager.cs
+++ Assets/Scripts/Ben_Scripts/InventoryManager.cs
@@ -3,9 +3,40 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class InventoryManager : MonoBehaviour
-{
-
+{ 
     public Inventory inventory;
     public EquipmentCraftingPanel equipmentPanel;
+
+    private void Start()
+    {
+        inventory.OnItemRightClickedEvent += AddToCraft;
+    }
+
+    public void AddToCraft(Item item)
+    {
+        if(inventory.RemoveItem(item))
+        {
+            Item previousItem;
+            if (equipmentPanel.AddItem(item, out previousItem))
+            {
+                if (previousItem != null)
+                {
+                    inventory.AddItem(previousItem);
+                }
+            }
+            else
+            {
+                inventory.AddItem(item);
+            }
+        }
+    }
+
+    public void RemoveFromCraft(Item item)
+    {
+        if(!inventory.IsFull() && equipmentPanel.RemoveItem(item))
+        {
+            inventory.AddItem(item);
+        }
+    }
 
 }
